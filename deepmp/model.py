2fe2a33765310cc066debd0ece9887ae3a1491d3
leopardShark <lamicccc@gmail.com>
2020-04-02 13:16:41 +0200

    embedding retrived

--- deepmp/model.py
+++ deepmp/model.py
@@ -7,17 +7,18 @@ import tensorflow as tf
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import *
 
-def get_lstm_model(base_num, embedding_size):
+def get_lstm_model(base_num):
 
-    embedded_bases = Input(shape=(base_num,embedding_size))
+    embedded_bases = Input(shape=(base_num,))
     means = Input(shape=(base_num,))
     stds = Input(shape=(base_num,))
     sanums = Input(shape=(base_num,))
 
-    vector = tf.concat([embedded_bases, tf.reshape(means, [-1, base_num, 1]),
-                                        tf.reshape(stds, [-1, base_num, 1]),
-                                        tf.reshape(sanums, [-1, base_num, 1])], 
-                                        axis=2)
+    vector = tf.concat([tf.reshape(embedded_bases, [-1, base_num, 1]),
+                                    tf.reshape(means, [-1, base_num, 1]),
+                                    tf.reshape(stds, [-1, base_num, 1]),
+                                    tf.reshape(sanums, [-1, base_num, 1])],
+                                    axis=2)
 
     x = Bidirectional(RNN([LSTMCell(100, dropout=0.2), \
         LSTMCell(100, dropout=0.2),LSTMCell(100, dropout=0.2)]))(vector)
@@ -27,7 +28,7 @@ def get_lstm_model(base_num, embedding_size):
     out = Dense(1, activation='sigmoid', use_bias=False)(x)
     model = Model([embedded_bases, means, stds, sanums], outputs=out)
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(), 
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
         loss='binary_crossentropy', metrics=['acc'])
     print(model.summary())
 
@@ -54,10 +55,10 @@ def get_cnn_model():
     return model
 
 
-#TODO <PB, MC> Need to obtain a proper joint model 
-def joint_model(event_output, signal_output, error_output): 
+#TODO <PB, MC> Need to obtain a proper joint model
+def joint_model(event_output, signal_output, error_output):
     joint_input = tf.concat(
-        [event_output, signal_output, error_output], axis=1) 
+        [event_output, signal_output, error_output], axis=1)
 
     joint_input_shape = joint_input.get_shape().as_list()
     model = tf.keras.Sequential()
--- deepmp/train.py
+++ deepmp/train.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-import os
+
 import time
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import datetime
-from tensorflow.keras.callbacks import EarlyStopping
 from deepmp.utils import kmer2code
 from deepmp.model import *
 
 def preprocess(csv_file, vocab_size, embedding_size):
-
-    weight_table = tf.compat.v1.get_variable(
-        "embedding", shape=[vocab_size, embedding_size], dtype=tf.float32,
-        initializer=tf.compat.v1.truncated_normal_initializer(stddev=np.sqrt(2. / vocab_size)))
 
     df = pd.read_csv(csv_file, delimiter = "\t",names = ['chrom','pos',
                                 'strand','pos_in_strand','readname','read_strand',
@@ -22,20 +17,16 @@ def preprocess(csv_file, vocab_size, embedding_size):
     df = df.dropna()
 
     kmer = df['k_mer'].apply(kmer2code)
-    embedded_bases = tf.nn.embedding_lookup(
-        weight_table, tf.dtypes.cast(np.stack(kmer), tf.int32)
-    )
-
     base_mean = [tf.strings.to_number(i.split(','), tf.float32) \
         for i in df['signal_means'].values]
     base_std = [tf.strings.to_number(i.split(','), tf.float32) \
         for i in df['signal_stds'].values]
     base_signal_len = [tf.strings.to_number(i.split(','), tf.float32) \
         for i in df['signal_lens'].values]
-    
+
     label = df['methy_label']
 
-    return embedded_bases, np.stack(base_mean), np.stack(base_std), \
+    return np.stack(kmer), np.stack(base_mean), np.stack(base_std), \
         np.stack(base_signal_len), label
 
 
@@ -72,16 +63,15 @@ def train_errors(train_file, val_file):
 
     X_train, Y_train = preprocess_errors(train_file, feat=20)
     X_val, Y_val  = preprocess_errors(val_file, feat=20)
-    
+
     model = get_cnn_model()
 
     log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S_errors")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
         log_dir=log_dir, histogram_freq=1
     )
- 
+
     model.fit(X_train, Y_train, batch_size=512, epochs=12,
                             callbacks = [tensorboard_callback],
                             validation_data=(X_val, Y_val))
-    return 
-
+    return None
