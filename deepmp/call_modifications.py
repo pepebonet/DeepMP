69557f9e8685dde8e293c79d255b6f532ccdbd1d
josebonet <jose.bonet@irbbarcelona.org>
2020-06-08 16:49:23 +0200

    first approach to position calling (on dev)

--- deepmp/call_modifications.py
+++ deepmp/call_modifications.py
@@ -8,6 +8,7 @@ from sklearn.metrics import precision_recall_fscore_support
 
 import deepmp.utils as ut
 import deepmp.plots as pl
+import deepmp.preprocess as pr
 
 
 def acc_test_single(data, labels, model_file, score_av='binary'):
@@ -15,14 +16,14 @@ def acc_test_single(data, labels, model_file, score_av='binary'):
     test_loss, test_acc = model.evaluate(data, tf.convert_to_tensor(labels))
 
     pred =  model.predict(data).flatten()
-    inferred = np.zeros(len(pred))
-    inferred[np.argwhere(pred > 0.5)] = 1
+    inferred = np.zeros(len(pred), dtype=int)
+    inferred[np.argwhere(pred >= 0.5)] = 1
 
     precision, recall, f_score, _ = precision_recall_fscore_support(
         labels, inferred, average=score_av
     )
 
-    return [test_acc, precision, recall, f_score]
+    return [test_acc, precision, recall, f_score], pred, inferred
 
 
 def get_accuracy_joint(inferred, err_pred, seq_pred, labels, score_av='binary'):
@@ -75,6 +76,12 @@ def save_output(acc, output):
     df.to_csv(os.path.join(output, 'accuracy_measurements.txt'), index=False, sep='\t')
 
 
+def save_output_positions(acc, output):
+    col_names = ['Accuracy', 'Precision', 'Recall', 'F-score']
+    df = pd.DataFrame([acc], columns=col_names)
+    df.to_csv(os.path.join(output, 'accuracy_measurements_positions.txt'), index=False, sep='\t')
+
+
 def save_probs(probs, labels, output):
     out_probs = os.path.join(output, 'test_pred_prob.txt')
     probs_to_save = pd.DataFrame(columns=['labels', 'probs'])
@@ -83,14 +90,70 @@ def save_probs(probs, labels, output):
     probs_to_save.to_csv(out_probs, sep='\t', index=None)
 
 
+def pred_site(df, pred_label, meth_label):
+    comb_pred = df.pred_prob.mean()
+    if comb_pred >= 0.5:
+        pred_label.append(1)
+    else: 
+        pred_label.append(0)
+    meth_label.append(df.methyl_label.unique()[0])
+
+    return pred_label, meth_label
+
+
+def pred_site_2(df, pred_label, meth_label):
+    comb_pred = df.pred_prob.min() + df.pred_prob.max()
+    if comb_pred >= 1:
+        pred_label.append(1)
+    else: 
+        pred_label.append(0)
+    meth_label.append(df.methyl_label.unique()[0])
+    # import pdb;pdb.set_trace()
+    return pred_label, meth_label
+
+
+def option_1(l):
+    cc = pd.concat([l[l['pred_prob'] > 0.9], l[l['pred_prob'] < 0.1]])
+    comb_pred = cc.pred_prob.mean()
+
+
+def do_per_position_analysis(df):
+    df['id'] = df['chrom'] + '_' + df['pos'].astype(str)
+    meth_label = []; pred_label = []
+    for i, j in df.groupby('id'):
+        if len(j.methyl_label.unique()) > 1:
+            for k, l in j.groupby('methyl_label'):
+                if len(l) > 0:
+                    pred_label, meth_label = pred_site_2(l, pred_label, meth_label)
+        else:
+            if len(j) > 0:
+                pred_label, meth_label = pred_site_2(j, pred_label, meth_label)
+            
+    precision, recall, f_score, _ = precision_recall_fscore_support(
+        meth_label, pred_label, average='binary'
+    )
+    import pdb; pdb.set_trace()
+
+
 def call_mods(model, test_file, model_err, model_seq, one_hot_embedding, 
     kmer_sequence, output, figures=False):
 
     if model == 'seq':
+
+        if test_file.rsplit('.')[-1] == 'tsv':
+            test = pd.read_csv(test_file, sep='\t', nrows=1100000,
+                names=['chrom', 'pos', 'strand', 'pos_in_strand', 'readname', 
+                'read_strand', 'kmer', 'signal_means', 'signal_stds', 'signal_lens', 
+                'cent_signals', 'methyl_label']) 
+            pr.preprocess_sequence(test, os.path.dirname(test_file), 'test')
+            test_file = os.path.join(os.path.dirname(test_file), 'test_seq.h5')
+
         data_seq, labels = ut.get_data_sequence(
             test_file, kmer_sequence, one_hot_embedding
         )
-        acc = acc_test_single(data_seq, labels, model_seq)
+        acc, pred, inferred = acc_test_single(data_seq, labels, model_seq)
+        test['pred_prob'] = pred; test['inferred_label'] = inferred
+        do_per_position_analysis(test)
 
     elif model == 'err':
         data_err, labels = ut.load_error_data(test_file)
