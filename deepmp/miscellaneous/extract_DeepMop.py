f3eaf76baa6bac5039262d724becc8d5b388dfb5
josebonet <jose.bonet@irbbarcelona.org>
2020-06-16 12:26:55 +0200

    DeepMod accuracy extended to input test set

--- deepmp/miscellaneous/extract_DeepMop.py
+++ deepmp/miscellaneous/extract_DeepMop.py
@@ -4,6 +4,9 @@ import h5py
 import click
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.metrics import precision_recall_fscore_support
 
@@ -15,10 +18,10 @@ def get_label(treatment):
         return 1 
 
 
-def get_reads_info(fast5, index, label):
+def get_reads_info(fast5, index, label, test=None):
     ind = pd.read_csv(index, sep=' ', header=None).values[:, [3,4]]
 
-    true = [] ; pred = []
+    true = [] ; pred = []; df_test = pd.DataFrame()
     with h5py.File(fast5, 'r') as hf:
         for el in ind: 
             data = hf['pred/{}/predetail'.format(el[0])][:]
@@ -28,21 +31,109 @@ def get_reads_info(fast5, index, label):
             chunks = np.asarray(list(generator))
 
             df = pd.DataFrame(data[np.argwhere(chunks == 'CG')].flatten())
+            
+            if test is not None: 
+                test_label = test[test['methyl_label'] == label]
+                merged = pd.merge(df, test_label, how='inner', 
+                    left_on='refbasei', right_on='pos')
+                if not merged.empty:
+                    pred.append(merged['mod_pred'].values)
+                    true.append(merged['methyl_label'].values)
+                    df_test = pd.concat([df_test, merged])
 
-            if label == 1:
-                pred.append(df['mod_pred'].values)
-                true.append(np.ones(len(df['mod_pred'].values)))
-                import pdb;pdb.set_trace()
             else:
-                pred.append(df['mod_pred'].values)
-                true.append(np.zeros(len(df['mod_pred'].values)))
-    return true, pred
+                if label == 1:
+                    pred.append(df['mod_pred'].values)
+                    true.append(np.ones(len(df['mod_pred'].values)))
+                else:
+                    pred.append(df['mod_pred'].values)
+                    true.append(np.zeros(len(df['mod_pred'].values)))
+
+    if test is not None:
+        return true, pred, df_test
+    else:
+        return true, pred
 
 
 def slice_chunks(l, n):
     for i in range(0, len(l) - n + 1):
         yield l[i:i + n]
 
+
+def pred_site(df, pred_label, meth_label, threshold=0.2):
+    inferred = df['mod_pred'].values
+    if np.sum(inferred) / len(inferred) >= threshold:
+        pred_label.append(1)
+    else: 
+        pred_label.append(0)
+    meth_label.append(df.methyl_label.unique()[0])
+
+    return pred_label, meth_label
+
+
+def do_per_position_analysis(df, output):
+    df['id'] = df['chrom'] + '_' + df['pos'].astype(str)
+    meth_label = []; pred_label = []; cov = []; new_df = pd.DataFrame()
+    pred_label_cov = []
+    for i, j in df.groupby('id'):
+        if len(j.methyl_label.unique()) > 1:
+            for k, l in j.groupby('methyl_label'):
+                if len(l) > 0:
+                    pred_label, meth_label = pred_site(l, pred_label, meth_label)
+                    cov.append(len(l))
+        else:
+            if len(j) > 0:
+                pred_label, meth_label = pred_site(j, pred_label, meth_label)
+                cov.append(len(j))
+            
+    precision, recall, f_score, _ = precision_recall_fscore_support(
+        meth_label, pred_label, average='binary'
+    )
+    
+    accuracy_cov(pred_label, meth_label, cov, output)
+    accuracy = get_accuracy_pos(meth_label, pred_label)
+    save_output(
+        [accuracy, precision, recall, f_score], output, 'position_accuracy.txt'
+    )
+
+
+def get_accuracy_pos(meth_label, pred_label):
+    pos = np.argwhere(np.asarray(meth_label) == 1)
+    neg = np.argwhere(np.asarray(meth_label) == 0)
+
+    pred_pos = np.asarray(pred_label)[pos]
+    pred_neg = np.asarray(pred_label)[neg]
+
+    accuracy = (sum(pred_pos) + len(pred_neg) - sum(pred_neg)) / \
+        (len(pred_pos) + len(pred_neg)) 
+    return accuracy[0]
+
+
+def accuracy_cov(pred, label, cov, output):
+    df_dict = {'predictions': pred, 'methyl_label': label, 'Coverage': cov}
+    df = pd.DataFrame(df_dict)
+    cov = []; acc = []
+
+    for i, j in df.groupby('Coverage'):
+        cov.append(i)
+        acc.append(get_accuracy_pos(
+            j['methyl_label'].tolist(), j['predictions'].tolist())
+        )
+    
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    sns.barplot(cov, acc)
+    ax.set_ylim(0.7,1)
+    fig.tight_layout()
+    
+    plt.savefig(os.path.join(output, 'acc_vs_cov.png'))
+    plt.close()
+
+
+def save_output(acc, output, label):
+    col_names = ['Accuracy', 'Precision', 'Recall', 'F-score']
+    df = pd.DataFrame([acc], columns=col_names)
+    df.to_csv(os.path.join(output, label), index=False, sep='\t')
 
 
 @click.command(short_help='Convert DeepMod output into accuracy scores.')
@@ -59,31 +150,47 @@ def slice_chunks(l, n):
     help='Index file containing read names.'
 )
 @click.option(
+    '-tf', '--test_file', default='', 
+    help='Test file to save only given positions.'
+)
+@click.option(
     '-o', '--output', default='', help='output folder'
 )
-def main(detect_folder, file_id, label, output):
+def main(detect_folder, file_id, label, output, test_file):
+
+    if test_file:
+        test = pd.read_csv(test_file, sep='\t', nrows=11000,
+            names=['chrom', 'pos', 'strand', 'pos_in_strand', 'readname', 
+            'read_strand', 'kmer', 'signal_means', 'signal_stds', 'signal_lens', 
+            'cent_signals', 'methyl_label'])
+        test['pos'] = test.pos.astype(int)
     
     treat_true = []; treat_pred = []
     untreat_true = []; untreat_pred = []
+    test_all = pd.DataFrame()
     treatments = os.listdir(detect_folder)
     for treat in treatments:
         label = get_label(treat)
         detect_subfolder = os.path.join(detect_folder, treat, file_id)
         detect_reads = os.listdir(detect_subfolder)
-        for el in detect_reads: 
+        for el in tqdm(detect_reads): 
             if os.path.isdir(os.path.join(detect_subfolder, el)):
                 read_folder = os.path.join(detect_subfolder, el)
                 reads = os.listdir(read_folder)
 
-                for i in range(int(len(reads)/2)):
+                for i in tqdm(range(int(len(reads)/2))):
                     fast5 = os.path.join(
                         read_folder, 'rnn.pred.detail.fast5.{}'.format(i)
                     )
                     index = os.path.join(
                         read_folder, 'Chromosome.rnn.pred.ind.{}'.format(i)
                     )
-                    true, pred = get_reads_info(fast5, index, label)
-                    # import pdb;pdb.set_trace()
+                    if test_file: 
+                        true, pred, df_test = get_reads_info(fast5, index, label, test)
+                        test_all = pd.concat([test_all, df_test]) 
+                    else:
+                        true, pred = get_reads_info(fast5, index, label)
+
                     if label == 1:
                         treat_true.append(np.concatenate(true))
                         treat_pred.append(np.concatenate(pred))
@@ -102,8 +209,15 @@ def main(detect_folder, file_id, label, output):
     precision, recall, f_score, _ = precision_recall_fscore_support(
         true, pred, average='binary'
     )
+    accuracy = get_accuracy_pos(true, pred)
+    save_output(
+        [accuracy, precision, recall, f_score], output, 'accuracy_all.txt'
+    )
     print(precision, recall, f_score)
-    import pdb;pdb.set_trace()
+
+    if test_file:
+        do_per_position_analysis(test_all, output)
+    # import pdb;pdb.set_trace()
 
 
 if __name__ == '__main__':
