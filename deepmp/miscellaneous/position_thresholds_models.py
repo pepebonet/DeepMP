a11100ce2ad899ec7bddec808f6132dce932f5e0
jbonet <jbonet@login01>
2020-12-17 11:16:16 +0100

    comparison with other methods included

--- deepmp/miscellaneous/position_thresholds_models.py
+++ deepmp/miscellaneous/position_thresholds_models.py
@@ -4,6 +4,8 @@ import click
 import numpy as np
 import pandas as pd
 from scipy.special import gamma
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 from sklearn.metrics import precision_recall_fscore_support
 
 read1_pos0 = 0.005  
@@ -12,6 +14,8 @@ fp = 0.001
 fn = 0.001  
 beta_a = 1
 beta_b = 5
+
+names_deepsignal = ['chrom', 'pos', 'strand', 'pos_in_strand', 'readname', 't/c', 'pred_unmod', 'pred_prob', 'inferred_label', 'kmer']
 
 def pred_site_all(df, pred_min_max, pred_005, pred_01, pred_02, pred_03, pred_04,  meth_label):
 
@@ -101,7 +105,7 @@ def beta_stats(obs_reads, pred_beta, prob_beta_mod, prob_beta_unmod):
     return pred_beta, prob_beta_mod, prob_beta_unmod
 
 
-def do_per_position_analysis(df, output):
+def do_per_position_analysis(df):
 
     cov = []; pred_min_max = []; pred_005 = []; pred_01 = []; pred_02 = []
     pred_03 = []; pred_04 = []; meth_label = []; ids = []; pred_posterior = []
@@ -151,21 +155,140 @@ def do_per_position_analysis(df, output):
 
     return preds
 
+
+def get_ids(paths):
+    ids = []
+    for path in paths: 
+        df = pd.read_csv(path, sep='\t')
+        df['id_per_read'] = df['chrom'] + '_' + df['pos'].astype(str) + '_' + df['readname']
+        ids.append((df[['id_per_read', 'methyl_label']], path.rsplit('/', 1)[1].split('_')[0]))
+
+    return ids
+
+
+def extract_preds_deepmp(predictions_file, output):
+    q1, median, q3 = [], [], []
+    q1_fp, median_fp, q3_fp = [], [], []
+    q1_fn, median_fn, q3_fn = [], [], []
+    label = []
+    for file in predictions_file:
+        test = pd.read_csv(file, sep='\t')
+        import pdb;pdb.set_trace()
+        label.append(file.rsplit('/', 1)[1].split('_')[0])
+
+        all_preds = do_per_position_analysis(test)
+        Q1, med, Q3 = np.percentile(all_preds['meth_freq_diff'].values, [25, 50, 75])
+        q1.append(Q1); median.append(med); q3.append(Q3)
+        Q1_fp, med_fp, Q3_fp = np.percentile(all_preds['fp_freq'].values, [25, 50, 75])
+        q1_fp.append(Q1_fp); median_fp.append(med_fp); q3_fp.append(Q3_fp)
+        Q1_fn, med_fn, Q3_fn = np.percentile(all_preds['fn_freq'].values, [25, 50, 75])
+        q1_fn.append(Q1_fn); median_fn.append(med_fn); q3_fn.append(Q3_fn)
+        
+        print(all_preds['meth_freq_diff'].mean(), all_preds['meth_freq_diff'].std())
+        print(all_preds['fp_freq'].mean(), all_preds['fp_freq'].std())
+        print(all_preds['fn_freq'].mean(), all_preds['fn_freq'].std())
+    
+    return (q1, median, q3, label)
+
+
+def extract_preds_deepsignal(predictions_file, output):
+    q1, median, q3 = [], [], []
+    q1_fp, median_fp, q3_fp = [], [], []
+    q1_fn, median_fn, q3_fn = [], [], []
+    label = []
+    for file in predictions_file:
+        test = pd.read_csv(file, sep='\t', header=None, names=names_deepsignal)
+        test['id'] = test['chrom'] + '_' + test['pos'].astype(str)
+        
+        label.append(file.rsplit('/', 1)[1].split('_')[0])
+        import pdb;pdb.set_trace()
+        all_preds = do_per_position_analysis(test)
+        Q1, med, Q3 = np.percentile(all_preds['meth_freq_diff'].values, [25, 50, 75])
+        q1.append(Q1); median.append(med); q3.append(Q3)
+        Q1_fp, med_fp, Q3_fp = np.percentile(all_preds['fp_freq'].values, [25, 50, 75])
+        q1_fp.append(Q1_fp); median_fp.append(med_fp); q3_fp.append(Q3_fp)
+        Q1_fn, med_fn, Q3_fn = np.percentile(all_preds['fn_freq'].values, [25, 50, 75])
+        q1_fn.append(Q1_fn); median_fn.append(med_fn); q3_fn.append(Q3_fn)
+    
+    return (q1, median, q3, label)
+
+
+def extract_preds_deepmod(predictions_file, output):
+    q1, median, q3 = [], [], []
+    q1_fp, median_fp, q3_fp = [], [], []
+    q1_fn, median_fn, q3_fn = [], [], []
+    label = []
+    for file in predictions_file:
+        test = pd.read_csv(file, sep='\t')
+        import pdb;pdb.set_trace()
+
+        label.append(file.rsplit('/', 1)[1].split('_')[0])
+
+        all_preds = do_per_position_analysis(test)
+        Q1, med, Q3 = np.percentile(all_preds['meth_freq_diff'].values, [25, 50, 75])
+        q1.append(Q1); median.append(med); q3.append(Q3)
+        Q1_fp, med_fp, Q3_fp = np.percentile(all_preds['fp_freq'].values, [25, 50, 75])
+        q1_fp.append(Q1_fp); median_fp.append(med_fp); q3_fp.append(Q3_fp)
+        Q1_fn, med_fn, Q3_fn = np.percentile(all_preds['fn_freq'].values, [25, 50, 75])
+        q1_fn.append(Q1_fn); median_fn.append(med_fn); q3_fn.append(Q3_fn)
+    
+    return (q1, median, q3, label)
+
+
+def plot_please(Q1, median, Q3, label, output):
+
+    fig, ax = plt.subplots(figsize=(5, 5), facecolor='white')
+
+    yerr = np.array([[np.asarray(median) - np.asarray(Q1)], [np.asarray(Q3) - np.asarray(median)]])
+    yerr = yerr.reshape(2, yerr.shape[2])
+
+    plt.errorbar(
+        np.asarray(label).astype(np.int), np.asarray(median), yerr=yerr, 
+        marker='o', mfc='#08519c', mec='black', ms=10, mew=1, 
+        ecolor='black', capsize=2.5, elinewidth=1, capthick=1
+    )
+
+    ax.set_xlabel("Methylation percentage", fontsize=12)
+    ax.set_ylabel("Inferred Frequency - True Frequency", fontsize=12)
+    plt.xticks(rotation=0)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    plt.ylim(top=0.1, bottom=-0.01)
+    plt.xlim(left=-1, right=102)
+
+    plt.tight_layout()
+    out_dir = os.path.join(output, 'methylation_frequency.pdf')
+    plt.savefig(out_dir)
+    plt.close()
+
+
 @click.command(short_help='Convert DeepMod output into accuracy scores.')
 @click.option(
-    '-pf', '--predictions_file', required=True, 
-    help='Folder containing the predictions on test'
+    '-pd', '--predictions_deepmp', multiple=True,
+    help='Folder containing the predictions on test from deepmp'
+)
+@click.option(
+    '-pds', '--predictions_deepsignal', multiple=True,
+    help='Folder containing the predictions on test from deepsignal'
+)
+@click.option(
+    '-pdm', '--predictions_deepmod', multiple=True,
+    help='Folder containing the predictions on test from deepmod'
 )
 @click.option(
     '-o', '--output', default='', help='output folder'
 )
-def main(predictions_file, output):
-    test = pd.read_csv(predictions_file, sep='\t')
-    all_preds = do_per_position_analysis(test, output)
-    print(all_preds['meth_freq_diff'].mean(), all_preds['meth_freq_diff'].std())
-    print(all_preds['fp_freq'].mean(), all_preds['fp_freq'].std())
-    print(all_preds['fn_freq'].mean(), all_preds['fn_freq'].std())
+def main(predictions_deepmp, predictions_deepsignal, predictions_deepmod, output):
+    ids = get_ids(predictions_deepmp)
+    preds_deepmp = extract_preds_deepmp(predictions_deepmp, output)
+    preds_deepsignal = extract_preds_deepsignal(predictions_deepsignal, output, ids)
+    # preds_deepmod = extract_preds_deepmod(predictions_deepmod, output)
     import pdb;pdb.set_trace()
+    plot_please(q1, median, q3, label, output)
+    import pdb;pdb.set_trace()
+
 
 if __name__ == '__main__':
     main()
