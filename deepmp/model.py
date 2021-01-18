f84ba49caf4c27d9c418c7887b8d7ea2a9a33f88
leopardShark <lamicccc@gmail.com>
2021-01-18 12:47:34 +0100

    Update model.py
    name changed and rawsignalNN

--- deepmp/model.py
+++ deepmp/model.py
@@ -8,9 +8,9 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 
 class ConvBlock(Model):
 
-    def __init__(self, units, filters):
+    def __init__(self, filters, kernel_size):
         super(ConvBlock, self).__init__()
-        self.conv = Conv1D(units, filters, padding="same",
+        self.conv = Conv1D(filters, kernel_size, padding="same",
                             kernel_initializer="lecun_normal")
         self.bn = BatchNormalization()
         self.relu = ReLU()
@@ -23,9 +23,9 @@ class ConvBlock(Model):
 
 class LocalBlock(Model):
 
-    def __init__(self, units, filters):
+    def __init__(self, filters, kernel_size):
         super(LocalBlock, self).__init__()
-        self.localconv = LocallyConnected1D(units, filters,
+        self.localconv = LocallyConnected1D(filters, kernel_size,
                                             kernel_initializer="lecun_normal")
         self.bn = BatchNormalization()
         self.relu = ReLU()
@@ -38,13 +38,13 @@ class LocalBlock(Model):
 
 class ConvLocalBlock(Model):
 
-    def __init__(self, units, filters):
+    def __init__(self, filters, kernel_size):
         super(ConvLocalBlock, self).__init__()
-        self.conv = Conv1D(units, filters, padding="same",
+        self.conv = Conv1D(filters, kernel_size, padding="same",
                             kernel_initializer="lecun_normal")
         self.bn1 = BatchNormalization()
         self.relu1 = ReLU()
-        self.localconv = LocallyConnected1D(units, filters)
+        self.localconv = LocallyConnected1D(filters, kernel_size)
         self.bn2 = BatchNormalization()
         self.relu2 = ReLU()
 
@@ -60,18 +60,18 @@ class ConvLocalBlock(Model):
 
 class SequenceCNN(Model):
 
-    def __init__(self, cnn_block, block_num, units, filters):
+    def __init__(self, cnn_block, block_num, filters, kernel_size):
         super(SequenceCNN, self).__init__()
         self.block_num = block_num
         if cnn_block == 'conv':
             for i in range(self.block_num):
-                setattr(self, "block%i" % i, ConvBlock(units, filters))
+                setattr(self, "block%i" % i, ConvBlock(filters, kernel_size))
         elif cnn_block == 'local':
             for i in range(self.block_num):
-                setattr(self, "block%i" % i, LocalBlock(units, filters))
+                setattr(self, "block%i" % i, LocalBlock(filters, kernel_size))
         else:
             for i in range(self.block_num):
-                setattr(self, "block%i" % i, ConvLocalBlock(units, filters))
+                setattr(self, "block%i" % i, ConvLocalBlock(filters, kernel_size))
         self.pool = GlobalAveragePooling1D(name='seq_pooling_layer')
         self.dense1 = Dense(256, activation='relu')
         self.dense2 = Dense(1, activation='sigmoid', use_bias=False)
@@ -89,15 +89,15 @@ class SequenceCNN(Model):
 
 class BCErrorCNN(Model):
 
-    def __init__(self, conv_blocks, local_blocks, units, filters):
+    def __init__(self, conv_blocks, local_blocks, filters, kernel_size):
         super(BCErrorCNN, self).__init__()
         self.conv_blocks = conv_blocks
         self.local_blocks = local_blocks
         for i in range(self.conv_blocks):
-            setattr(self, "cblock%i" % i, ConvBlock(units, filters))
+            setattr(self, "cblock%i" % i, ConvBlock(filters, kernel_size))
         self.maxpool = MaxPooling1D()
         for i in range(self.local_blocks):
-            setattr(self, "lblock%i" % i, LocalBlock(units, filters))
+            setattr(self, "lblock%i" % i, LocalBlock(filters, kernel_size))
         self.avgpool = GlobalAveragePooling1D(name='err_pooling_layer')
         self.dense1 = Dense(256, activation='relu')
         self.dense2 = Dense(1, activation='sigmoid', use_bias=False)
@@ -187,12 +187,8 @@ class RawSigNN(Model):
 
     def __init__(self, rnn_cell):
         super(CentralBaseNN, self).__init__(rnn_cell)
-        if rnn_cell == "gru":
-            self.brnn = Bidirectional(RNN([GRUCell(256, dropout=0.2), \
+        self.brnn = Bidirectional(RNN([GRUCell(256, dropout=0.2), \
                             GRUCell(256, dropout=0.2),GRUCell(256, dropout=0.2)]))
-        else:
-            self.brnn = Bidirectional(RNN([LSTMCell(256, dropout=0.2), \
-                            LSTMCell(256, dropout=0.2),LSTMCell(256, dropout=0.2)]))
         self.fc = Dense(1, activation='sigmoid',use_bias=False)
 
     def call(self, inputs, submodel=False):
