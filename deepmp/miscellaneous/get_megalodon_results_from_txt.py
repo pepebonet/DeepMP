9dd34da6c3b96ee4485bca09e26e9a82d6cba37e
josebonet <jose.bonet@irbbarcelona.org>
2021-06-10 14:28:55 +0200

    label option added

--- deepmp/miscellaneous/get_megalodon_results_from_txt.py
+++ deepmp/miscellaneous/get_megalodon_results_from_txt.py
@@ -25,13 +25,20 @@ import deepmp.utils as ut
     help='dict reads'
 )
 @click.option(
+    '-ml', '--methyl_label', default='', 
+    help='whether to include the methyl lable as an additional column'
+)
+@click.option(
     '-o', '--output', default='', help='output path'
 )
-def main(megalodon_path, dict_reads, output):
+def main(megalodon_path, dict_reads, methyl_label, output):
     megalodon = pd.read_csv(megalodon_path, sep='\t')
 
     megalodon['mod_prob'] = np.exp(megalodon['mod_log_prob'])
     megalodon['unmod_prob'] = np.exp(megalodon['can_log_prob'])
+
+    if methyl_label:
+        megalodon['methyl_label'] = int(methyl_label)
 
     if dict_reads: 
         dict_names = ut.load_obj(dict_reads)  
