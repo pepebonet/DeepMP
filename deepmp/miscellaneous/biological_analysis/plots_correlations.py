537f0dcc26656801bdcf84ac4958a5da138f0ad5
josebonet <jose.bonet@irbbarcelona.org>
2021-09-16 18:08:08 +0200

    include dotplots

--- deepmp/miscellaneous/biological_analysis/plots_correlations.py
+++ deepmp/miscellaneous/biological_analysis/plots_correlations.py
@@ -155,7 +155,7 @@ def do_heatmap(imp, line1, output):
     plt.close()
 
 
-def do_boxplot_comparisons(df, label, plot_type, output):
+def do_boxplot_comparisons(df, reg, corr, label, plot_type, output):
 
     df['Method_Frequency'] = df['Method_Frequency'].astype(int)
 
@@ -188,9 +188,16 @@ def do_boxplot_comparisons(df, label, plot_type, output):
             count += 1
 
     for me in ['DeepMP', 'DeepSignal', 'Nanopolish', 'Guppy']:
+        
+        lab = '{} (r = {}, r² = {}, RMSE = {}'.format(
+            me, np.round(reg[me][2], 3), 
+            np.round(corr[corr['Method'] == me]['Pearson'].values[0], 3), 
+            np.round(corr[corr['Method'] == me]['RMSE'].values[0], 3)
+        )
+        
         custom_lines.append(
             plt.plot([],[], marker="o", ms=7, ls="", mec='black', 
-            mew=0, color=palette_dict[me], label=me)[0] 
+            mew=0, color=palette_dict[me], label=lab)[0] 
         )
 
     b.spines['top'].set_visible(False)
@@ -198,20 +205,95 @@ def do_boxplot_comparisons(df, label, plot_type, output):
     plt.legend().set_visible(False)
 
     b.legend(
-        bbox_to_anchor=(0., 1., 1., .102),
+        bbox_to_anchor=(0., 1.2, 1., .102),
         handles=custom_lines, loc='upper center', 
-        facecolor='white', ncol=2, fontsize=8, frameon=False
+        facecolor='white', ncol=1, fontsize=8, frameon=False
     )
 
-    b.set_xlabel("Method Methylation Frequency", fontsize=12)
-    b.set_ylabel("Bisulfite Methylation Frequency", fontsize=12)
+    b.set_xlabel("Method Methylation", fontsize=12)
+    b.set_ylabel("Bisulfite Methylation", fontsize=12)
 
-    import pdb;pdb.set_trace()
     out_fig = os.path.join(output, 'comparison_{}.pdf'.format(label))
     plt.tight_layout()
     plt.savefig(out_fig)
     plt.close()
+
+
+def do_dotplot_comparisons(df, reg, corr, label, output):
+
+    df['Method_Frequency'] = df['Method_Frequency'].astype(int)
+
+    #init the figure
+    fig, ax = plt.subplots(figsize=(5, 5), facecolor='white')
+    custom_lines = []
+
+    for pos in df['Method_Frequency'].unique():
+        for me in ['DeepMP', 'DeepSignal', 'Nanopolish', 'Guppy']:
+            
+            vals = df[(df['Method_Frequency'] == pos) & (df['Method'] == me)]
+            bis_val = vals['Bisulfite_Frequency'].values
+            Q1, med, Q3 = np.percentile(bis_val, [25, 50, 75])
+
+            yerr = np.array([[med - Q1], [Q3 - med]])
+            # import pdb;pdb.set_trace()
+
+            if me == 'DeepMP':
+                p = -3
+            if me == 'DeepSignal':
+                p = -1
+            if me == 'Nanopolish':
+                p = 1
+            if me == 'Guppy':
+                p = 3
+
+            plt.errorbar(pos + p, med, yerr=yerr, 
+                marker='o', mfc=palette_dict[me], mec='black', ms=5, 
+                mew=0.5,  ecolor='black', capsize=1, capthick=0.5, elinewidth=0.8, 
+                c=palette_dict[me]
+            )
+
+    for me in ['DeepMP', 'DeepSignal', 'Nanopolish', 'Guppy']:
+
+        x_reg, y_reg, r2, slope, intercept = reg[me]
+        y_reg_set = list(dict.fromkeys(y_reg).keys())
+        x_reg_set = list(dict.fromkeys(x_reg.flatten()).keys())
+        x_reg_set.insert(0, 0); y_reg_set.insert(0, intercept)
+        plt.plot(
+            x_reg_set, y_reg_set, color=palette_dict[me], linewidth=1, 
+            alpha= 0.6, ls='-'             
+        )
+        
+        lab = '{} (r = {}, r² = {}, RMSE = {}'.format(
+            me, 
+            np.round(corr[corr['Method'] == me]['Pearson'].values[0], 3), 
+            np.round(reg[me][2], 3), 
+            np.round(corr[corr['Method'] == me]['RMSE'].values[0], 3)
+        )
+        
+        custom_lines.append(
+            plt.plot([],[], marker="o", ms=7, ls="", mec='black', 
+            mew=0, color=palette_dict[me], label=lab)[0] 
+        )
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.legend().set_visible(False)
+
+    ax.legend(
+        bbox_to_anchor=(0., 1.2, 1., .102),
+        handles=custom_lines, loc='upper center', 
+        facecolor='white', ncol=1, fontsize=8, frameon=False
+    )
+
+    ax.set_xlabel("Method Methylation", fontsize=12)
+    ax.set_ylabel("Bisulfite Methylation", fontsize=12)
+
+    out_fig = os.path.join(output, 'comparison_dotplot_{}.pdf'.format(label))
+    plt.tight_layout()
+    plt.savefig(out_fig)
+    plt.close()
     
+
 
 def linear_regression(df):
     
@@ -269,8 +351,8 @@ def main(imprinting_results, line1_results, imprinting_freq, line1_freq, output)
     reg_imp = linear_regression(imp_freq)
     reg_line1 = linear_regression(line1_freq)
     #TODO include violin and then dot plot see which one is better
-    do_boxplot_comparisons(imp_freq, reg_imp, imp, 'Imprinting Genes', 'boxplot', output)
-    do_boxplot_comparisons(line1_freq, reg_line1, line1, 'LINE1', 'boxplot', output)
+    do_dotplot_comparisons(imp_freq, reg_imp, imp, 'Imprinting Genes', output)
+    do_dotplot_comparisons(line1_freq, reg_line1, line1, 'LINE1', output)
 
     do_heatmap(imp, line1, output)
  
